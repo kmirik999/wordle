a7e9edbf52acc9b46907b2306f73ec1c6ff940b2
Myroslav Kryvoruchko <90711690+kmirik999@users.noreply.github.com>
2023-01-18 17:17:45 +0200

    refactor, and fixed the bugs

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <utility>
 #include <vector>
 #include <string>
 #include <algorithm>
@@ -9,137 +10,139 @@
 
 using namespace std;
 
+class Wordle {
+private:
+    int WordLength;
+    string filePath;
+    const int NotMatched = 0;
+    const int PartialMatch = 1;
+    const int Match = 2;
+    const int NumberOfGuesses = 6;
+    vector<vector<int>> matches;
+    vector<string> guesses;
 
-int WordLength = 5;
-string filePath;
-const int NotMatched= 0;
-const int PartialMatch = 1;
-const int Match = 2;
-const int NumberOfGuesses = 6;
-
-void toUpperCase(string &input){
-    transform(input.begin(), input.end(), input.begin(), [](unsigned char c){return toupper(c);});
-}
-
-string getRandomWord()
-{/*
-    std::string word;
-    std::string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    std::random_device rd;
-    std::mt19937 generator(rd());
-    std::uniform_int_distribution<int> distribution(0, alphabet.length() - 1);
-    for(int i = 0; i < WordLength; i++)
-    {
-        word += alphabet[distribution(generator)];
-    }
-    return word;
-    */
-    int i = 0;
-    vector<string> GenerateRandomWord ;
-    string word1;
-
-    random_device rd;
-    mt19937 gen(rd());
-
-
-    cout << filePath<< endl;
-    ifstream fileRandomNames(filePath);
-    if (!fileRandomNames.is_open()) {
-        cout << "Error: unable to open file" << endl;
-        return "";
+public:
+    int getNumberOfGuesses() {
+        return NumberOfGuesses;
     }
 
-    string line;
-
-    while (getline(fileRandomNames, line))
-    {
-        istringstream iss(line);
-
-        iss >> word1;
-
-        GenerateRandomWord.push_back(word1);
-        i++;
+    int getWordLength() {
+        return WordLength;
     }
-    uniform_int_distribution<> disRandomProduct(0, i-1);
-    int id;
-    do{
-        id = disRandomProduct(gen);
-    } while (GenerateRandomWord.at(id).length() != WordLength);
-        return GenerateRandomWord.at(id);
-}
+    void setWordLength(int newWordLength) {
+        WordLength = newWordLength;
+    }
+    string getFilePath() {
+        return filePath;
+    }
+    void setFilePath(string newFilePath) {
+        filePath = std::move(newFilePath);
+    }
+    Wordle() {}
+    Wordle(int wordLength, string filePath) : WordLength(wordLength), filePath(move(filePath)) {}
 
+     void toUpperCase(string& input) {
+        transform(input.begin(), input.end(), input.begin(), [](unsigned char c) { return toupper(c); });
+     }
 
-bool isValidWord(const string& word){
-    return word.length() == WordLength && word.find_first_not_of("ABCDEFGHIJKLMNOPQRSTUVWXYZ") == string::npos;
-}
-
-
-void markMatches(vector<vector<int>> &matches, int currentGuessIndex, string targetWord, string guessWord)
-{
-    cout<< targetWord << endl;
-
-    for (int i = 0; i < guessWord.length(); i++)
+    string getRandomWord()
     {
-        for(int j = 0; j < targetWord.length(); j++)
+        int i = 0;
+        vector<string> GenerateRandomWord;
+        string word1;
+
+        random_device rd;
+        mt19937 gen(rd());
+
+
+        cout << filePath << endl;
+        ifstream fileRandomNames(filePath);
+        if (!fileRandomNames.is_open()) {
+            cout << "Error: unable to open file" << endl;
+            return "";
+        }
+
+        string line;
+
+        while (getline(fileRandomNames, line))
         {
-            if (matches[currentGuessIndex][i] == NotMatched && guessWord[i] == targetWord[j])
+            istringstream iss(line);
+
+            iss >> word1;
+
+            GenerateRandomWord.push_back(word1);
+            i++;
+        }
+        uniform_int_distribution<> disRandomProduct(0, i - 1);
+        int id;
+        do {
+            id = disRandomProduct(gen);
+        } while (GenerateRandomWord.at(id).length() != WordLength);
+        string result = GenerateRandomWord.at(id);
+        toUpperCase(result);
+        return result;
+    }
+
+    bool isValidWord(const string& word) const {
+        return word.length() == WordLength && word.find_first_not_of("ABCDEFGHIJKLMNOPQRSTUVWXYZ") == string::npos;
+    }
+
+    void markMatches(vector<vector<int>>& matches, int currentGuessIndex, string targetWord, string guessWord) const
+    {
+        cout << targetWord << endl;
+        for (int i = 0; i < guessWord.length(); i++)
+        {
+            for (int j = 0; j < targetWord.length(); j++)
             {
-                if(i == j)
+                if (/*matches[currentGuessIndex][i] == NotMatched &&*/ guessWord[i] == targetWord[j])
                 {
-                    matches[currentGuessIndex][i] = Match;
-                    break;
-                }
-                else
-                {
-                    matches[currentGuessIndex][i] = PartialMatch;
+                    if (i == j)
+                    {
+                        matches[currentGuessIndex][i] = Match;
+                        break;
+                    }
+                    else
+                    {
+                        matches[currentGuessIndex][i] = PartialMatch;
+                    }
                 }
             }
         }
     }
-}
-bool isAllMatched(string targetWord, string guessWord)
-{
-    for (int i = 0; i <targetWord.length();i++)
-    {
-        if(targetWord[i] != guessWord[i])
-            return false;
-    }
-    return true;
-}
-void printMatchedWord(vector<vector<int>> &matches, string targetWord, int currentGuessIndex)
-{
-    for(int i = 0; i < targetWord.length(); i++)
-    {
-        if(matches[currentGuessIndex][i] == Match)
-        {
-            cout << targetWord[i];
+
+    static bool isAllMatched(string targetWord, string guessWord) {
+        for (int i = 0; i < targetWord.length(); i++) {
+            if (tolower(targetWord[i]) != tolower(guessWord[i]))
+                return false;
         }
+        return true;
     }
-    cout << endl;
-}
 
-void printWordle(vector<vector<int>> matches, vector<string> guesses, int currentGuessIndex)
-{
-    for (int i = 0; i <= currentGuessIndex && guesses.size(); i++) {
-        std::string separator = "-";
-        std::string padding = "|";
-        std::string text = "|";
 
-        for (int j = 0; j < guesses[i].length(); ++j) {
-            separator += "------";
-            padding += "     |";
-            char value = toupper(guesses[i][j]);
-            text += " ";
-            if (matches[i][j] == PartialMatch) {
-                text += "\033[33m";
-            } else if (matches[i][j] == Match) {
-                text += "\033[32m";
+    void printWordle(vector<vector<int>> matches, vector<string> guesses, int currentGuessIndex)
+    {
+        for (int i = 0; i <= currentGuessIndex; i++) {
+            std::string separator = "-";
+            std::string padding = "|";
+            std::string text = "|";
+
+            for (int j = 0; j < guesses[i].length(); ++j) {
+                separator += "------";
+                padding += "     |";
+                char value = toupper(guesses[i][j]);
+                text += " ";
+                if (matches[i][j] == PartialMatch) {
+                    text += "\033[33m";
+                } else if (matches[i][j] == Match) {
+                    text += "\033[32m";
+                }
+                text += value;
+                if (matches[i][j] == PartialMatch || matches[i][j] == Match) {
+                    text += "\033[0m";
+                }
+                text += "   |";
+
             }
-            text += value;
-            if (matches[i][j] == PartialMatch || matches[i][j] == Match) {
-                text += "\033[0m";
-            }
-            text += "   |";
             if (i == 0) {
                 std::cout << separator << std::endl;
             }
@@ -149,56 +152,58 @@ void printWordle(vector<vector<int>> matches, vector<string> guesses, int curren
             std::cout << separator << std::endl;
         }
     }
-}
+};
 
 
 int main() {
     int selection;
-    cout <<  "Enter 1 --- Easy  --- 5 letters word"<< endl;
-    cout <<  "Enter 2 --- Medium --- 6 letters word"<< endl;
-    cout <<  "Enter 3 --- Hard --- 9 letters word"<< endl;
-    cout << "Enter the level(1, 2, 3):"<< endl;
+    Wordle w = Wordle();
+    cout << "Enter 1 --- Easy  --- 5 letters word" << endl;
+    cout << "Enter 2 --- Medium --- 7 letters word" << endl;
+    cout << "Enter 3 --- Hard --- 9 letters word" << endl;
+    cout << "Enter the level(1, 2, 3):" << endl;
     cin >> selection;
     switch (selection) {
         case 1:
-            WordLength = 5;
-            filePath = "data.txt";
+
+            w.setWordLength(5);
+            w.setFilePath("data.txt");
             break;
         case 2:
-            WordLength = 6;
-            filePath = "data1.txt";
+            w.setWordLength(7);
+            w.setFilePath("data1.txt");
             break;
         case 3:
-            WordLength = 9;
-            filePath = "data2.txt";
+            w.setWordLength(9);
+            w.setFilePath("data2.txt");
             break;
     }
-    string targetWord = getRandomWord();
+    string targetWord = w.getRandomWord();
     vector<string> guesses;
-    vector<vector<int>> matches(NumberOfGuesses, vector<int>(WordLength));
+    vector<vector<int>> matches(w.getNumberOfGuesses(), vector<int>(w.getWordLength()));
     int currentGuessIndex = -1;
     while (true) {
         string guess;
-        cout << "Enter your guess (word length must be " << WordLength << ") or type Q to quit the game: ";
+        cout << "Enter your guess (word length must be " << w.getWordLength() << ") or type Q to quit the game: ";
         cin >> guess;
         if (guess == "Q" || guess == "q") {
             break;
         }
-        toUpperCase(guess);
-        if (!isValidWord(guess)) {
-            cout << "Invalid word. Please enter a valid word of length " << WordLength << endl;
+        w.toUpperCase(guess);
+        if (!w.isValidWord(guess)) {
+            cout << "Invalid word. Please enter a valid word of length " << w.getWordLength() << endl;
             continue;
         }
         currentGuessIndex++;
         guesses.push_back(guess);
-        markMatches(matches, currentGuessIndex, targetWord, guess);
-        printWordle(matches, guesses, currentGuessIndex);
-        if (isAllMatched(targetWord,guess)) {
+        w.markMatches(matches, currentGuessIndex, targetWord, guess);
+        w.printWordle(matches, guesses, currentGuessIndex);
+        if (Wordle::isAllMatched(targetWord, guess)) {
             cout << "Congratulations! You won the game!" << endl;
             break;
         }
-        if(currentGuessIndex == NumberOfGuesses - 1){
-            cout << "Sorry, you lose the game! The word was: "<< targetWord << endl;
+        if (currentGuessIndex == w.getNumberOfGuesses() - 1) {
+            cout << "Sorry, you lose the game! The word was: " << targetWord << endl;
             break;
         }
     }
